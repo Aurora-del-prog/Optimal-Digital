ba070564986b3c5bcf2528810ae4a078343b853e
Cc <2744166358@qq.com>
2023-03-11 15:23:34 +0800

    CDN

--- vue.config.js
+++ vue.config.js
@@ -14,10 +14,18 @@ module.exports = defineConfig({
     },
   },
   // transpileDependencies: true,
-  //本地是否能调试，看到源码
+  //调试是否能看到源码
   productionSourceMap:true,
-    // 删除预加载
   chainWebpack:(config) =>{
+    // 删除预加载
     config.plugins.delete('prefetch');
   },
+  // chunk-vendors.db740b4e.js由180k-->112k
+  // 防止将某些import的包打包到bundle中，而是在运行时再去外部获取依赖，CDN引入
+  configureWebpack: {
+    externals: {
+      vue: 'Vue',
+      'element-plus': 'ElementPlus'
+    }
+  }
 });
